65a5bc9ed15708c69e651c743d2570ae2303ae79
IIEternityII <timofey18082005@gmail.com>
2021-11-26 18:59:59 +0300

    add str

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ class Note:
     PITCHES = ['до', 'ре', 'ми',
                'фа', 'соль', 'ля', 'си']
     LPITCHES = ['до-о', 'ре-э', 'ми-и', 'фа-а', 'со-оль', 'ля-а', 'си-и']
+
+    def str(self):
+        if self.d:
+            return self.LPITCHES[self.ind]
+        else:
+            return self.PITCHES[self.ind]
