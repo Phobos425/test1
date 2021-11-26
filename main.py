a108d2a92bb96824cea41b41691183e097ea67d6
kaks <makbloks@gmail.com>
2021-11-26 18:52:37 +0300

    add init

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ class Note:
     PITCHES = ['до', 'ре', 'ми',
                'фа', 'соль', 'ля', 'си']
     LPITCHES = ['до-о', 'ре-э', 'ми-и', 'фа-а', 'со-оль', 'ля-а', 'си-и']
+
+    def init(self, note, d=False):
+        self.d = d
+        self.ind = self.PITCHES.index(note)
